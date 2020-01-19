d558fb1c383782fa1fba2230348a36e6e186245c
Bill Gauvey <bgauvey@gmail.com>
2020-01-19 08:16:31 -0500

    debug stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,10 +16,16 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     const config = {
+      // tslint:disable-next-line: max-line-length
       accessToken: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXIiOiIwLjIuMCIsIndjbiI6IlBvd2VyQmlBenVyZVNhbXBsZXMiLCJ3aWQiOiJmODFjMTk2Ni1lZGVlLTQxMWItOGY4YS1mODQ0NjAxOWIwNDQiLCJyaWQiOiJjNTJhZjhhYi0wNDY4LTQxNjUtOTJhZi1kYzM5ODU4ZDY2YWQiLCJpc3MiOiJQb3dlckJJU0RLIiwiYXVkIjoiaHR0cHM6Ly9hbmFseXNpcy53aW5kb3dzLm5ldC9wb3dlcmJpL2FwaSIsImV4cCI6MTg5MzQ0ODgwMCwibmJmIjoxNDgxMDM3MTY5fQ.m4SwqmRWA9rJgfl72lEQ_G-Ijpw9Up5YwmBOfXi00YU',
       embedUrl:  'https://embedded.powerbi.com/appTokenReportEmbed?reportId=c52af8ab-0468-4165-92af-dc39858d66ad',
       id: 'c52af8ab-0468-4165-92af-dc39858d66ad',
-      type: 'report'
+
+
+      // id: 'c24ceac8-1f45-49a1-b82e-92aa0d4ebf1e', // User Owns Data
+      // secret: 'W0rT30MOjyO5sQWRh3rs/BzpAg8/tWXG1MKAhYJMcTE=' //User Owns Data
+      // id: '27846936-1a5d-4d80-8a74-33835f686588',
+      type: 'report',
   };
 
     // display the embedded report
